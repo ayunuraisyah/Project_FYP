ecd81c9ef5d348ac3570219df3c1c1e762e5d8a8
juniaiman <aimann1556@gmail.com>
2024-07-12 17:25:43 +0700

    rifad

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,12 +7,12 @@
 
         <form action="/admin" method="post" class="mt-2" enctype="multipart/form-data">
             @csrf
-            <div class="mb-4">
+            <div class="">
                 <input type="text" placeholder="Nama makanan" name="title" id="title"
                     class="w-full h-12 rounded-lg pl-3 bg-black bg-opacity-20 placeholder-white text-white focus:outline-none focus:bg-opacity-30">
             </div>
             <select name="origin" id="Daerah"
-                class="w-full h-12 rounded-lg mt-2 text-white bg-black bg-opacity-20 focus:outline-none focus:bg-opacity-30"
+                class="w-full h-12 rounded-lg mt-3 text-white bg-black bg-opacity-20 focus:outline-none focus:bg-opacity-30 "
                 style="text-indent: 5px;">
                 <option value="">Daerah Makanan</option>
                 <option value="Jawa">Jawa</option>
@@ -21,20 +21,20 @@
                 <option value="Sulawesi">Sulawesi</option>
                 <option value="Bali">Bali</option>
             </select>
-            <div class="mb-4 mt-2">
+            <div class=" mt-3">
                 <input type="number" name="price" id="Harga"
                     class="w-full h-12 rounded-lg pl-3 bg-black bg-opacity-20 placeholder-white text-white focus:outline-none focus:bg-opacity-30"
                     placeholder="Harga">
             </div>
             <input type="number" name="stock" id="Stock" placeholder="Stock"
-                class="w-full h-12 rounded-lg pl-3 bg-black bg-opacity-20 placeholder-white text-white focus:outline-none focus:bg-opacity-30">
-            <div class="mb-4 mt-2">
+                class="w-full h-12 rounded-lg pl-3 bg-black bg-opacity-20 placeholder-white text-white focus:outline-none focus:bg-opacity-30 mt-3">
+            <div class=" mt-3">
                 <input id="x" type="hidden" name="description">
                 <trix-editor input="x" class="w-full bg-black bg-opacity-20 border-0 text-white"></trix-editor>
             </div>
             <input type="hidden" name="favorite" value="0">
             <input type="hidden" name="sold" value="0">
-            <div class="mb-4 mt-2">
+            <div class=" mt-3">
                 <label for="Gambar"
                     class="block text-sm font-medium text-gray-700 text-center mb-1">Pilih Gambar</label>
                 <div class="flex items-center justify-center">
